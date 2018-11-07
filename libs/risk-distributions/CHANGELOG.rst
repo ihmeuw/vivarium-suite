a1601314e05249ba19e50665cfa674fa62566b04
Kate Wilson <kwilson7@uw.edu>
2018-11-07 15:51:17 -0800

    update versions for release

--- libs/risk-distributions/CHANGELOG.rst
+++ libs/risk-distributions/CHANGELOG.rst
@@ -1,3 +1,8 @@
+**0.1.1 - 11/07/18**
+
+    - Clean up separated distributions.
+    - Allow for only mean and standard deviation or pre-calculated parameters.
+
 **0.1.0 - 10/29/18**
 
  - Initial Release
--- libs/risk-distributions/src/vivarium/risk_distributions/__about__.py
+++ libs/risk-distributions/src/vivarium/risk_distributions/__about__.py
@@ -7,7 +7,7 @@ __title__ = "risk_distributions"
 __summary__ = "Components for building distributions. Compatible for use with ``vivarium``"
 __uri__ = "https://github.com/ihmeuw/risk_distributions"
 
-__version__ = "1.0.0"
+__version__ = "0.0.1"
 
 __author__ = "The risk_distributions developers"
 __email__ = "vivarium.dev@gmail.com"
