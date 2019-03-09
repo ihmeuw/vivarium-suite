cf365e8ff9435f622bbf61973f00f30acfe5d5d9
Kate Wilson <kwilson7@uw.edu>
2019-03-08 16:03:42 -0800

    don't forget cdf processing

--- libs/risk-distributions/src/vivarium/risk_distributions/risk_distributions.py
+++ libs/risk-distributions/src/vivarium/risk_distributions/risk_distributions.py
@@ -371,7 +371,7 @@ class MirroredGamma(BaseDistribution):
 
     def process(self, data: pd.Series, parameters: pd.DataFrame, process_type: str) -> pd.Series:
         x_min, x_max = parameters.loc[data.index, 'x_min'], parameters.loc[data.index, 'x_max']
-        if process_type == 'pdf_preprocess':
+        if process_type in ['pdf_preprocess', 'cdf_preprocess']:
             value = x_max - data
         elif process_type == 'ppf_preprocess':
             # noinspection PyTypeChecker
