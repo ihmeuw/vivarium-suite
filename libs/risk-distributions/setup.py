e233af64f6b74d531d1cebc6cdf518864a635311
James Collins <collijk@uw.edu>
2022-02-12 03:30:40 -0800

    Pin sphinx

--- libs/risk-distributions/setup.py
+++ libs/risk-distributions/setup.py
@@ -28,11 +28,8 @@ if __name__ == "__main__":
     ]
 
     doc_requirements = [
-        'sphinx',
-        'sphinx-autodoc-typehints',
-        'sphinx-rtd-theme',
-        'IPython',
-        'matplotlib'
+        "sphinx>=4.0",
+        "sphinx-rtd-theme",
     ]
 
     setup(
