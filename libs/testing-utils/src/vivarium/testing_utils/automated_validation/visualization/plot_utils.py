ccdf64dae8fd9f7d7b794b7dd64078c034f2d45b
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2026-07-15 13:21:54 -0700

    Apply suggestion from @patricktnast

--- libs/testing-utils/src/vivarium/testing_utils/automated_validation/visualization/plot_utils.py
+++ libs/testing-utils/src/vivarium/testing_utils/automated_validation/visualization/plot_utils.py
@@ -276,7 +276,6 @@ def _get_combined_data(
     """Get the combined data from the test and reference datasets."""
     test_data, reference_data = comparison.align_datasets(stratifications)
 
-    # Scenario levels were already filtered and dropped during bundle formatting.
 
     # Add input draw with placeholder if necessary
     if DRAW_INDEX in test_data.index.names and DRAW_INDEX not in reference_data.index.names:
