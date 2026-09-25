301ce0717be1b547f025cf47d20d27befafaf1ab
Patrick Nast <pnast@uw.edu>
2026-09-25 11:52:12 -0700

    add dataclass

---
+++ libs/cluster-tools/src/vivarium/cluster_tools/core/pytask/task.py
@@ -0,0 +1,17 @@
+"""An encapsulation of individual pipeline tasks."""
+
+from collections.abc import Callable
+from typing import Any
+from dataclasses import dataclass
+from pytask import PNode
+
+
+@dataclass
+class Task:
+    name: str
+    run: Callable[..., Any] | str
+    inputs: dict[str, PNode]
+    outputs: dict[str, PNode]
+    resources: dict[str, Any]
+    env: str | None
+    code_id: PNode
